2972f17b45cf5a10cbfd93f716a00082a06c0b6d
newline-power-tools <doug@theironyard.com>
2017-08-15 14:51:40 +0000

    dcb25b1f-iterator_basics.zip

---
+++ iterator_basics/starter_files/iterators.rb
@@ -0,0 +1,29 @@
+# Print all words in the array but uppercased.
+def print_capitals(words)
+end
+
+# Double all numbers in the array and return a new array.
+def double_it(numbers)
+end
+
+# Return an array of all even numbers in the given array.
+def find_evens(numbers)
+end
+
+# Return the product of all numbers
+# Do _NOT_ use numbers.product.
+def product(numbers)
+end
+
+# Get all odd numbers from the given array, square them, and
+# return a new array of those squares.
+def square_only_odds(numbers)
+end
+
+# Square all given numbers and sum them.
+def sum_of_squares(numbers)
+end
+
+# Return a new array of the ordinals (1 -> 1st) of the numbers given.
+def ordinals(numbers)
+end
